28d874158bb81a64fa0f21df7740d3bcdaa83455
samandcode <chijiokeemechebe@gmail.com>
2023-02-28 13:27:30 +0100

    Push

--- components/TopBar.js
+++ components/TopBar.js
@@ -14,7 +14,7 @@ export default function TopBar({ showNav, setShowNav }) {
                     className="h-7 w-7 text-gray-700 cursor-pointer"
                     onClick={() => setShowNav(!showNav)}
                 /> */}
-                <svg className="h-7 w-7 text-gray-700 cursor-pointer" onClick={() => setShowNav(!showNav)} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                {/* <svg className="h-7 w-7 text-gray-700 cursor-pointer" onClick={() => setShowNav(!showNav)} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <g id="style=linear">
                         <g id="menu-hotdog">
                             <path id="vector" d="M5 6H19" stroke="#1A1A1A" strokeWidth="1.5" stroke-linecap="round" />
@@ -22,6 +22,18 @@ export default function TopBar({ showNav, setShowNav }) {
                             <path id="vector_3" d="M5 18H19" stroke="#1A1A1A" strokeWidth="1.5" stroke-linecap="round" />
                         </g>
                     </g>
+                </svg> */}
+
+                <svg className={`h-5 w-5 text-gray-700 cursor-pointer` + (showNav ? "cursor-pointer transform -rotate-180" : "")}
+                    viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"
+                    onClick={() => setShowNav(!showNav)}
+                >
+                    <path
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                        d="M2.14645 11.1464C1.95118 11.3417 1.95118 11.6583 2.14645 11.8536C2.34171 12.0488 2.65829 12.0488 2.85355 11.8536L6.85355 7.85355C7.04882 7.65829 7.04882 7.34171 6.85355 7.14645L2.85355 3.14645C2.65829 2.95118 2.34171 2.95118 2.14645 3.14645C1.95118 3.34171 1.95118 3.65829 2.14645 3.85355L5.79289 7.5L2.14645 11.1464ZM8.14645 11.1464C7.95118 11.3417 7.95118 11.6583 8.14645 11.8536C8.34171 12.0488 8.65829 12.0488 8.85355 11.8536L12.8536 7.85355C13.0488 7.65829 13.0488 7.34171 12.8536 7.14645L8.85355 3.14645C8.65829 2.95118 8.34171 2.95118 8.14645 3.14645C7.95118 3.34171 7.95118 3.65829 8.14645 3.85355L11.7929 7.5L8.14645 11.1464Z"
+                        fill="#000000"
+                    />
                 </svg>
             </div>
             <div className="flex items-center gap-4 pr-4 md:pr-8">

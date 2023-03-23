574a891bce2b4bdd69da1ef25101c428beeca801
Ikechukwu Uzoma <uzomacharles28@outlook.com>
2023-03-23 05:21:50 +0100

    revert back to next middleware

--- middleware.js
+++ middleware.js
@@ -1,18 +1,15 @@
 import { NextResponse } from "next/server"
-import { MiddlewareRequest } from "@netlify/next";
 
 
 export default function middleware(req) {
-    const request = new MiddlewareRequest(req);
-    
     var myCookie = req.cookies.get('bcloudCookie')
 
     if (//req.nextUrl.pathname.startsWith('/jobs') || 
     req.nextUrl.pathname === '/') {
         if (!myCookie) {
-            const url = request.nextUrl.clone()// req.nextUrl.clone()
+            const url = req.nextUrl.clone()
             url.pathname = '/login'
-            return request.redirect(url)
+            return NextResponse.redirect(url)
         }
     }
 
